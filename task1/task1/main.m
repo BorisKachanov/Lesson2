cb9d50190ef6ec561388ef9699bd3b26398073d6
Kachanov <kachanov.boris96@gmail.com>
2015-10-19 13:39:38 -0700

    task 1 and task 2 is ready

--- task1/task1/main.m
+++ task1/task1/main.m
@@ -7,14 +7,51 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-int main(int argc, char * argv[]) {
-	// Task 1
-	
-	
-	// Task 2
-	
-	
-	// Task 3
-	return 0;
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        //Огласите и инициализируйте переменные всех фундаментальных типов которые сможете найти.
+        int i = 1;
+        float f = 3.7f;
+        double d = 8.44e+11;
+        char c = 'H';
+        long double l = 12.341;
+        //С помощью функции NSLog поочередно выведите свои переменные в консоль.
+        NSLog(@"Integer variable = %i", i);
+        NSLog(@"Float variable = %f", f);
+        NSLog(@"Double variable = %a", d);
+        NSLog(@"Char variable = %c", c);
+        NSLog(@"Long double variable = %Lf", l);
+        //Создайте три цикла (while-do do-while for) и с помощью функции NSLog выведите значения от 1 до 10 в консоль
+        
+        for(int i = 1; i <= 10; i++){
+            NSLog(@"%i", i);
+        }
+        
+        while (i <= 10){
+            NSLog(@"%i", i);
+            i++;
+        }
+        do{
+            NSLog(@"%i", i);
+            i++;
+        }while(i <= 10);
+        //Создайте экземпляры класса NSString наибольшым количеством способом которые сможете придумать (начинайте с [[NSString alloc] init], искать по ключевым словам initialization, NSString, class, allocation)
+        NSString *string1 = [[NSString alloc]init];
+        NSString *string2 = [NSString alloc];
+        [string2 init];
+        NSString *string3 = [NSString stringWithString: string1];
+        NSString *string4 = [[NSString alloc] initWithString: string1];
+        //Напишите любой код который приведет к крешу приложения (ключевые слова crash, stack trac, program termination)
+        /*NSString *string1 = [NSString alloc];
+         NSString *string2 = [NSString stringWithString: string1];
+         NSString *string3 = [[NSString alloc] initWithString: string2];
+         NSLog(@"%@", string3);*/
+        
+        //Напишите самый короткий код который приведет к крешу приложения.
+        /*NSString *string = [NSString alloc];
+         NSLog(@"%@", string);*/
+    }
+    return 0;
 }
--- task2/task2/main.m
+++ task2/task2/main.m
@@ -7,7 +7,58 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+@interface Card : NSObject
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic) NSInteger *value;
+@end
 
-int main(int argc, char * argv[]) {
-	return 0;
+@implementation Card
+@end
+
+@interface  PlayingCard : Card
+-(id) initWithArgs: (NSString *) nameArg andValueArg: (NSInteger) valueArg;
+@end
+
+@implementation PlayingCard
+
+@synthesize value = _value;
+-(NSInteger) value {
+    return *(_value + self.name.length);
 }
+-(id)initWithArgs: (NSString *) nameArg andValueArg: (NSInteger) valueArg{
+    self = [super init];
+    if (self){
+        self.name = nameArg;
+        self.value = valueArg;
+    }
+    return self;
+}
+
+int main(int argc, char * argv[])
+@autoreleasepool {
+    
+    Card *myCard1 = [[Card alloc]init];
+    myCard1.name = @"myCard #1";
+    myCard1.value = 1;
+    
+    Card *myCard2 = [[Card alloc]init];
+    myCard2.name = @"myCard #2";
+    myCard2.value = 2;
+    
+    Card *myCard3 = [[Card alloc]init];
+    myCard3.name = @"myCard #3";
+    myCard3.value = 3;
+    
+    PlayingCard *myPlayingCard1 = [[PlayingCard alloc]initWithArgs:@"myPlayingCard #1" andValueArg: 1];
+    PlayingCard *myPlayingCard2 = [[PlayingCard alloc]initWithArgs:@"myPlayingCard #2" andValueArg: 2];
+    PlayingCard *myPlayingCard3 = [[PlayingCard alloc]initWithArgs:@"myPlayingCard #3" andValueArg: 3];
+    
+    NSArray *array = @[myCard1, myCard2, myCard3, myPlayingCard1, myPlayingCard2, myPlayingCard3];
+    
+    for (Card *item in array){
+        NSLog(@"%i", item.value);
+    }
+    return 0;
+}
+@end
